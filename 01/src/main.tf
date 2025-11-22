653b78faa4ca98297ebb431af7b140dba60b013a
FunUndead <76656471+FunUndead@users.noreply.github.com>
2025-11-22 21:38:57 +0400

    hw_1

---
+++ 01/src/main.tf
@@ -0,0 +1,41 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 3.0.1"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.6.0"
+    }
+  }
+  required_version = ">= 1.12.0, < 1.15.0" /*Многострочный комментарий.
+ Требуемая версия terraform */
+}
+provider "docker" {}
+provider "random" {}
+
+resource "random_password" "random_string" {
+  length      = 16
+  special     = false
+  min_upper   = 1
+  min_lower   = 1
+  min_numeric = 1
+}
+
+
+resource "docker_image" "nginx" {
+  name         = "nginx:latest"
+  keep_locally = true
+}
+
+resource "docker_container" "nginx1" {
+  image = docker_image.nginx.image_id
+  name  = "hello_world" 
+
+  ports {
+    internal = 80
+    external = 9090
+  }
+}
+
